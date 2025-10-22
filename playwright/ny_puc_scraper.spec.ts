d4a48b31c0b65eec8474b380bbce70f8950d151f
Nicole Venner <nvenner@protonmail.ch>
2025-10-22 08:33:41 -0600

    fix actual typescript error

--- playwright/ny_puc_scraper.spec.ts
+++ playwright/ny_puc_scraper.spec.ts
@@ -647,7 +647,7 @@ class NyPucScraper {
 
     if (matterSubtype) {
       const prefix = "NEW Case Proceeding:";
-      if (matterSubtype.toLowercase().startsWith(prefix.toLowerCase())) {
+      if (matterSubtype.toLowerCase().startsWith(prefix.toLowerCase())) {
         matterSubtype = matterSubtype.slice(prefix.length).trim();
       }
       metadata.case_subtype = matterSubtype;
